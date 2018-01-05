ea8364ffb8663486398c60720c1cd642eed258eb
Ross Squires <ross.squires@gmail.com>
2018-01-05 14:11:18 -0500

    Update RNDefaultPreferencePackage.java

--- android/src/main/java/com/reactlibrary/RNDefaultPreferencePackage.java
+++ android/src/main/java/com/reactlibrary/RNDefaultPreferencePackage.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNDefaultPreferencePackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
@@ -19,5 +21,9 @@ public class RNDefaultPreferencePackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
     }
 }
